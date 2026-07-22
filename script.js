cd64bd9b0e331b2f453f07b93567de1f595e2621
Eduardo DIAZ <eadiaz6x@gmail.com>
2026-07-22 12:21:08 -0300

    Crear script.js

---
+++ script.js
@@ -0,0 +1,53 @@
+const MATERIAS = {
+  "m1": {
+    "materia_id": "m1",
+    "materia_name": "Jefe de Servicios",
+    "questions": [
+      // Todas las preguntas...
+    ]
+  }
+
+  // Continúan las demás materias...
+};
+
+const INDEX = {
+  "materias": [
+    {
+      "id": "m1",
+      "name": "Jefe de Servicios",
+      "file": "materia1.json"
+    },
+    {
+      "id": "m2",
+      "name": "Jefe de Oficina Judicial",
+      "file": "materia2.json"
+    },
+    {
+      "id": "m3",
+      "name": "CPPCABA",
+      "file": "materia3.json"
+    },
+    {
+      "id": "m4",
+      "name": "Guía de Actuación Policial",
+      "file": "materia4.json"
+    },
+    {
+      "id": "m5",
+      "name": "Régimen Disciplinario",
+      "file": "materia5.json"
+    },
+    {
+      "id": "m6",
+      "name": "Oficina de Administración",
+      "file": "materia6.json"
+    },
+    {
+      "id": "m7",
+      "name": "Mando y Conducción",
+      "file": "materia7.json"
+    }
+  ]
+};
+
+// Continúa aquí todo el JavaScript que tenías dentro de la etiqueta script.
